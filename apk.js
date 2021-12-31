d48992e8047603407910ea90f26dfc3f632de2e9
elmurodxxx <elmurodx98@gmail.com>
2021-12-31 10:43:58 +0500

    first commit

---
+++ apk.js
@@ -0,0 +1,175 @@
+let ism='Elmurod';
+console.log(ism);
+
+let familiya='Khojimatov';
+console.log(familiya);
+
+let yosh='23';
+console.log(yosh);
+
+let x = 5;
+let y = 2;
+let z = x * y+5;
+console.log(z)
+
+
+// 1
+let a=3; b=4;
+let p=4*a;
+console.log(p)
+// 2
+let s=a*a;
+console.log(s)
+// 3
+let S=a*b;
+let P=2*(a+b);
+console.log(S)
+console.log(P)
+// 4
+let d=5; 
+const п=3;
+let L=п*d;
+console.log(L)
+// 5
+console.log(Math.pow(a,3));
+console.log(6*a*a);
+// 6
+let c=5;
+let V=a*b*c;
+let s1=2*(a*b+b*c+a*c);
+console.log(V);
+console.log(s1);
+// 7
+let R=1;
+let L1=2*п*R;
+let S1=п*R*R;
+console.log(L1);
+console.log(S1);
+// 8
+console.log((a+b)/2);
+// 9
+console.log(Math.sqrt(a+b));
+// 10
+console.log(a+b);
+console.log(a*b);
+console.log(Math.pow(a,2));
+console.log(Math.pow(b,2));
+// 11
+console.log(Math.abs(a));
+console.log(Math.abs(b));
+// 12
+console.log(Math.sqrt(Math.pow(a,2)+Math.pow(b,2)));
+console.log(a+b+c);
+// 13
+let R2=2; R1=3;
+console.log(п*R1);
+console.log(п*R2);
+console.log(п*(R1-R2));
+// 14
+let L2=15;
+console.log(L2/(2*п));
+console.log(п*3*3);
+// 15
+let S2=16;
+console.log(Math.sqrt(S2/п))
+// 16
+let X1=3; X2=17;
+console.log(Math.abs(X1-X2));
+// 17
+let AC=15; AB=11;
+console.log(AC+AB);
+// 18
+let AD=13; BD=8;
+console.log(AD*BD);
+// 19
+let k=5; f=7;
+console.log(2*(k+f));
+console.log(k*f);
+// 20
+let x1=6; x2=2; y1=7; y2=4;
+console.log(Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2)))
+// 21
+let m=2; j=5; q=7;
+let p4=(m+j+q)/2;
+console.log(p4);
+console.log(Math.sqrt(p4*(p4-m)*(p4-j)*(p4-q)));
+// 22
+let h=9; g=5;
+console.log(h);
+console.log(g);
+// 23
+let w=1; e=8; l=3;
+console.log(w);
+console.log(e);
+console.log(l);
+// 24
+let t=2; y=4; u=0;
+console.log(t);
+console.log(y);
+console.log(u);
+// 25
+console.log(Math.pow(3*x,6)-Math.pow(6*x,2)-7);
+// 26
+console.log(Math.pow(4*(x-3),6)-Math.pow(7*(x-3),3)+2);
+// 27
+let A=3;
+console.log(Math.pow(A,2));
+console.log(Math.pow(A,4));
+console.log(Math.pow(A,8));
+// 28
+console.log(Math.pow(A,2));
+console.log(Math.pow(A,3));
+console.log(Math.pow(A,5));
+console.log(Math.pow(A,10));
+console.log(Math.pow(A,15));
+// 29
+console.log(90*Math.PI/180);
+// 30
+console.log(180/2);
+// 31
+let Tf=150;
+let Tc=(Tf-32)*5/9;
+console.log(Tc);
+// 32
+let tC=155;
+let tF=(tC-32)*5/9;
+console.log(tF);
+// 33
+let narx=p; massa=m; massa1=f;
+console.log(narx/massa);
+console.log((narx/massa)*massa1);
+// 34
+let narx1=1500; mass1=3; narx2=2000; mass2=5;
+console.log(narx1/mass1);
+console.log(narx2/mass2);
+console.log((narx1/mass1)-(narx2/mass2));
+// 35
+let Ud=2; Vq=6; T1=1; T2=3;
+let S3=T1*(Vq+Ud);
+let S4=T2*(Vq-Ud);
+console.log(S3);
+console.log(S4);
+// 36
+let S5=12; Tu=1; V1q=4; V2q=6;
+let S6=Tu*(V1q+V2q)+S5;
+console.log(S6);
+// 37
+let S7=Tu*(V2q-V1q);
+console.log(S7);
+// 38
+let B=21;
+console.log(-B/A);
+// 39
+let C=14;
+let D=Math.pow(B,2)-4*A*C;
+let X5=(-B+Math.sqrt(D))/2*A;
+console.log(D);
+console.log(X5);
+// 40
+let A2=6; B2=42; C2=28;
+let D1=(A*B2-A2*B);
+let x6=(C*B2-C2*B)/D1;
+let y6=(A*C2-A2*C)/D1;
+console.log(D1);
+console.log(x6);
+console.log(y6);
